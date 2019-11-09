8607074d107688d01568e0f882926286013785c7
Haruki <khanh@trustingsocial.com>
2019-11-09 19:16:15 +1100

    Update version 2

--- app.py
+++ app.py
@@ -1,17 +1,46 @@
 import os
 import json
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 app = Flask(__name__)
 uploads_dir = os.path.join(app.instance_path, 'data')
+questions_dir = os.path.join(app.instance_path, 'questions')
 os.makedirs(uploads_dir, exist_ok=True)
+os.makedirs(questions_dir, exist_ok=True)
+
+
+@app.route('/a', methods=["GET"])
+def edit_answer():
+    qid = request.args.get('qid', default='0')
+    topic = request.args.get('t', default='misc')
+    question_path = os.path.join(questions_dir, topic + '.' + str(qid) + '.json')
+    question_info = {'question': 'yy', 'answer': 'xx'}
+    if os.path.exists(question_path):
+        question_info = json.load(open(question_path))
+
+    return render_template('answer.html', qid=qid, topic=topic, question_info=question_info)
+
+
+@app.route('/va', methods=["GET"])
+def view_answer():
+    qid = request.args.get('qid', default='2')
+    topic = request.args.get('t', default='misc')
+    question_path = os.path.join(questions_dir, topic + '.' + qid + '.json')
+    question_info = {'question': 'yy', 'answer': 'xx'}
+    if os.path.exists(question_path):
+        question_info = json.load(open(question_path))
+
+    return render_template('view_answer.html', qid=qid, topic=topic, question_info=question_info)
 
 
 @app.route('/v', methods=["GET"])
 def view():
     word = request.args.get('w', default='teacher')
+    return_type = request.args.get('r', default='html')
     fp = open(os.path.join(app.instance_path, 'data/' + word + '.json'), 'r')
     word_info = json.load(fp)
-    return render_template('view.html', word_info=word_info)
+    if return_type == 'html':
+        return render_template('view.html', word_info=word_info)
+    return jsonify(word_info)
 
 
 @app.route('/e', methods=["GET"])
@@ -38,6 +67,22 @@ def save():
         return 'data is none'
     fp = open(os.path.join(app.instance_path, 'data/' + word + '.json'), 'w')
     fp.write(data)
+    fp.close()
+    return 'We saved it. Thanks.'
+
+
+@app.route('/qa', methods=["POST"])
+def save_answer():
+    topic = request.form.get('t', default=None)
+    qid = request.form.get('qid', default=None)
+    question = request.form.get('q', default=None)
+    answer = request.form.get('a', default=None)
+    if topic is None or qid is None or question is None or answer is None:
+        return 'something is none'
+    question_path = os.path.join(questions_dir, topic + '.' + qid + '.json')
+    question_info = {'question': question, 'answer': answer}
+    fp = open(question_path, 'w')
+    json.dump(question_info, fp)
     fp.close()
     return 'We saved it. Thanks.'
 
